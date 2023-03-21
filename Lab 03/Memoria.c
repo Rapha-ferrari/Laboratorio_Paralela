483b61b9a6d139c8a8feda6025f0755263b1a007
Raphael Ferrari <64502502+Rapha-ferrari@users.noreply.github.com>
2023-03-21 08:19:32 -0300

    Update Memoria.c

--- Lab 03/Memoria.c
+++ Lab 03/Memoria.c
@@ -1,1 +1,40 @@
+#include  <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 
+
+int main(){
+
+    pid_t childpid;
+    int shm_id; 
+
+    shm_id = shmget(IPC_PRIVATE, 1024, IPC_CREAT |  0660);
+         if (shm_id < 0) {
+             printf("shmget error\n");
+        }
+    int *shrd = (int*)shmat(shm_id, NULL, 0);
+
+
+    *shrd = 1;
+
+  
+    printf("O valor inicial da variavel compartilhada é %i\n",*shrd);
+    childpid = fork();
+    printf("o childpid é %i\n",childpid);
+    if (childpid == 0){     
+        *shrd+= 2;  
+
+        printf("O valor da variavel compartilhada ao somar 2 é %i\n",*shrd);    
+    }
+    else{ 
+        wait(NULL);     
+        *shrd *= 4 ;
+
+        printf("O valor da variavel compartilhada ao multiplicar por 4 é %i\n",*shrd);    
+
+    }
+
+
+    return 0;
+}
