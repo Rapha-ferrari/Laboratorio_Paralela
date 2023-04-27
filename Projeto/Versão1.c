a239d206aaefa0ea145581f23a69139dd9769be8
Raphael Ferrari <64502502+Rapha-ferrari@users.noreply.github.com>
2023-04-27 19:20:08 -0300

    Create Versão1.c

---
+++ Projeto/Versão1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Cálculo da série de Taylor iterativamente.
+// A função incrementa o valor de i de 1..t
+// para calcular a soma de 1/1 até 1/t
+
+double iSerieTaylor(int t)
+{
+  double soma = 0;
+  for (double i = 1; i <= t; i++)
+    {
+      soma += 1/i;
+    }
+  return soma;
+}
+
+int main() 
+{
+  double soma = iSerieTaylor(10);
+  printf("-> ln(10)\nSoma = %f\n", soma);
+  return 0;
+}
